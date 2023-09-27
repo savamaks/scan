bdeb26e0e60a7d908bca3db2e8b9eda34f83d5b1
Максим <105449753+savamaxxx@users.noreply.github.com>
2023-09-27 13:41:59 +0300

    удалил часть компонентов

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -6,7 +6,6 @@ import SearchPage from "./page/SearchPage";
 import MenuBurger from "./MenuBurger";
 import { useAppSelector } from "../Reducer/store";
 import ResultPage from "./page/ResultPage";
-import Proba from "./page/Proba";
 
 const MainContainer = styled.main`
     grid-area: main;
@@ -19,19 +18,16 @@ const RoutesEnum = {
     AUTH: "/auth",
     SEARCH: "/search",
     RESULT: "/result",
-    PROBA:'/proba'
 };
 const routes = [
     { path: RoutesEnum.MAIN, component: <MainPage /> },
     { path: RoutesEnum.AUTH, component: <Authorization /> },
     { path: RoutesEnum.SEARCH, component: <SearchPage /> },
     { path: RoutesEnum.RESULT, component: <ResultPage /> },
-    { path: RoutesEnum.PROBA, component: <Proba /> },
-
 ];
 
 const Main = () => {
-    const activeBurger  = useAppSelector((state) => state.appSlice.activeBurger);
+    const activeBurger = useAppSelector((state) => state.appSlice.activeBurger);
     return (
         <MainContainer>
             {activeBurger && <MenuBurger />}
