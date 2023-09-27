d9955c6e874ee7542b45ada0c0e80f5be156153c
Максим <105449753+savamaxxx@users.noreply.github.com>
2023-09-27 16:48:57 +0300

    a2

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -6,7 +6,6 @@ import SearchPage from "./page/SearchPage";
 import MenuBurger from "./MenuBurger";
 import { useAppSelector } from "../Reducer/store";
 import ResultPage from "./page/ResultPage";
-import Proba from "./page/Proba";
 
 
 const MainContainer = styled.main`
@@ -20,14 +19,12 @@ const RoutesEnum = {
     AUTH: "/auth",
     SEARCH: "/search",
     RESULT: "/result",
-    PROBA:'/proba'
 };
 const routes = [
     { path: RoutesEnum.MAIN, component: <MainPage /> },
     { path: RoutesEnum.AUTH, component: <Authorization /> },
     { path: RoutesEnum.SEARCH, component: <SearchPage /> },
     { path: RoutesEnum.RESULT, component: <ResultPage /> },
-    { path: RoutesEnum.PROBA, component: <Proba /> },
 
 ];
 
